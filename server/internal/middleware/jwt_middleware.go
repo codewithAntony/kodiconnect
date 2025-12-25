14e776553a12ab99965b4e3feeac9d43be5cb293
kimongo <antonymurithi51@gmail.com>
2025-12-25 20:06:29 +0300

    created my middleware

---
+++ server/internal/middleware/jwt_middleware.go
@@ -0,0 +1,36 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWT(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("Authorization")
+		if !strings.HasPrefix(header, "Bearer ") {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(header, "Bearer ")
+
+		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		c.Set("user_id", int(claims["user_id"].(float64)))
+		c.Set("role", claims["role"])
+
+		c.Next()
+	}
+}
